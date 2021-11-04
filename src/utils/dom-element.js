8beac10ccafe27e9fc16cc1695ab44bae2e6110a
BuGalter <wardoc78@gmail.com>
2021-11-04 21:54:21 +0300

    Added to class constructor

--- src/utils/dom-element.js
+++ src/utils/dom-element.js
@@ -1,9 +1,19 @@
 /**
- * Dom-element module.
+ * dom-element module.
  * @module utils/dom-element
  */
 
 /** Class representing DOM element.*/
 class DomElement {
-  
+  /**
+   * Create a DOM Element.
+   * @param {object} elementDescription - Description of the DOM element being created. 
+   */
+  constructor(elementDescription) {
+    this.domElementTag = elementDescription.domElementTag;
+    this.domElementClasses = elementDescription.domElementClasses;
+    this.domElementStyle = elementDescription.domElementStyle;
+    this.domElementAttributes = elementDescription.domElementAttributes;
+    this.domElementText = elementDescription.domElementText;
+  };
 }
