84e095ce2757b0a08cf13344e5262b2d17229038
BuGalter <wardoc78@gmail.com>
2021-11-09 22:31:27 +0300

    Added to index.js some test code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './index.css';
+import { DomElement } from './utils/dom-element';
+
+const exampleElement = {
+  domElementTag: 'h1',
+  domElementClasses: [],
+  domElementStyle: {},
+  domElementAttributes: [],
+  domElementText: 'Hello, World!' 
+};
+
+const simpleObj = new DomElement(exampleElement);
+console.log(simpleObj);
+console.log(simpleObj.tag);
