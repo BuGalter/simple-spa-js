8fed122691ec49fe784083994ba6d16cd3456fa2
BuGalter <wardoc78@gmail.com>
2021-11-10 20:36:24 +0300

    Added object description DOM element to project

---
+++ src/components/app-description/app-description.js
@@ -0,0 +1,14 @@
+/**
+ * app-descpription module.
+ * @module simple-spa-js/app-descpription.
+ */
+
+
+/**
+ * @typedef {Object} DOM element.
+ * @property {string} domElementTag - tag name DOM node.
+ * @property {array} domElementClasses - array of element class name.
+ * @property {object} domElementStyle - the object that contains the styles of the element.
+ * @property {object} domElementAttributes - the object that contains the attributes of the element.
+ * @property {string} domElementText - element text content.
+ */
