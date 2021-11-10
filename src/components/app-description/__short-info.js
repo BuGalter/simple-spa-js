8d89124e17f64584b6a0e4608b17913366ef2508
BuGalter <wardoc78@gmail.com>
2021-11-10 21:09:21 +0300

    Added an object with a element description to describe the short info about app

---
+++ src/components/app-description/__short-info.js
@@ -0,0 +1,17 @@
+/**
+ * __short-info module.
+ * @module simple-spa-js/app-descpription/__short-info.
+ */
+
+export const shortInfo = {
+  domElementTag: 'h3',
+  domElementClasses: ['app-description__short-info'],
+  domElementStyle: {
+    'color': 'red;',
+  },
+  domElementAttributes: {
+    test: 'test h3',
+  },
+  domElementText: 'A simple single page application written in HTML, CSS, JS for working\
+  with the database mySQL' 
+};
