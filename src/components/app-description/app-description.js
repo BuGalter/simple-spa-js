89659f3885307981bfa40786f679cb9639b55b33
BuGalter <wardoc78@gmail.com>
2021-11-10 20:50:54 +0300

    Added an object with a container description to describe the app

--- src/components/app-description/app-description.js
+++ src/components/app-description/app-description.js
@@ -12,3 +12,18 @@
  * @property {object} domElementAttributes - the object that contains the attributes of the element.
  * @property {string} domElementText - element text content.
  */
+
+ const appDescription = {
+  domElementTag: 'div',
+  domElementClasses: ['app-description'],
+  domElementStyle: {
+    'flex': '1;',
+    'justify-content': 'center;',
+    'align-content': 'center;',
+    'background-color': 'springgreen;'
+  },
+  domElementAttributes: {
+    id: 'test',
+  },
+  domElementText: 'Hello, world!' 
+};
