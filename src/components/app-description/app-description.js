015c5773776b5e4e875db3418ad4ad760a303c0e
BuGalter <wardoc78@gmail.com>
2021-11-13 17:15:38 +0300

    Removed test code and added creation app description by using Creator

--- src/components/app-description/app-description.js
+++ src/components/app-description/app-description.js
@@ -1,4 +1,4 @@
-import { DomElement } from "../../utils/dom-element";
+import { Creator } from "../../utils/creator";
 import { shortInfo } from "./__short-info";
 
 /**
@@ -19,39 +19,15 @@ import { shortInfo } from "./__short-info";
     'flex': '1;',
     'background-color': 'springgreen;'
   },
-  /* domElementAttributes: {
+  domElementAttributes: {
     id: 'test',
-  }, */
+  },
   domElementText: 'Hello, world!' 
 };
 
-export function makeAppDescription() {
-  const domElement = new DomElement(appDescription);
-  const tag = domElement.tag;
-  console.log(tag);
-  const style = domElement.style;
-  //const style = undefined;
-  console.log(style);
-  const text = domElement.text;
-  console.log(text);
-  const attributes = domElement.attributes;
-  console.log(attributes);
-  const classes = domElement.classes;
-  //const classes = undefined;
-  console.log(classes);
-  const element = document.createElement(tag);
-  if (classes !== undefined) {
-    for (let i = 0; i < classes.length; i += 1 ) {
-      element.classList.add(classes[i]);
-    };
-  };
-  if (style !== undefined) {
-    element.setAttribute('style', style);
-  };
-  for (let prop in attributes) {
-    element.setAttribute(prop, attributes[prop]);
-  };
-  element.textContent = text;
-  let wrapper = document.querySelector('.wrapper-content');
-  wrapper.append(element);
+function makeAppDescription() {
+  const appDescribe = Creator.createDomElement(appDescription);
+  return appDescribe;
 };
+
+export { makeAppDescription };
