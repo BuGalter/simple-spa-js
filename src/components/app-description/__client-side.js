ed5e58f51e0d4995b4578390f170d08ef56afda9
BuGalter <wardoc78@gmail.com>
2021-11-14 21:23:10 +0300

    Added element client side to app description

---
+++ src/components/app-description/__client-side.js
@@ -0,0 +1,20 @@
+/**
+ * __client-side module.
+ * @module simple-spa-js/app-descpription/__client-side.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const clientSideElement = {
+  /**
+   * Item description short information about the client side of application.
+   * @type {DOM element}
+   */
+  domElementTag: 'p',
+  domElementClasses: ['app-description__client-side'],
+  domElementStyle: {
+    'color': 'blueviolet;',
+    'text-align': 'center;',
+  },
+  domElementText: 'На стороне клиента были использованны: html и css, все элементы были\
+  сгенерированы с помощью JavaScript' 
+};
