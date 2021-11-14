4fd4822fa73cc69f8b04af48580efe1e3f23dd13
BuGalter <wardoc78@gmail.com>
2021-11-14 20:30:26 +0300

    Added to app header element heading

---
+++ src/components/app-header/__heading.js
@@ -0,0 +1,15 @@
+/**
+ * __heading module.
+ * @module simple-spa-js/app-header/__heading.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const headingElement = {
+  /**
+   * Item description main heading app.
+   * @type {DOM element}
+   */
+  domElementTag: 'h1',
+  domElementClasses: ['app-header__heading'],
+  domElementText: 'Simple SPA  CSS, HTML, JS, MySql' 
+};
