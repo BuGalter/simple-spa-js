7aed12bb50551222ff9d0e0c82bd9a0d3aa6a5c0
BuGalter <wardoc78@gmail.com>
2021-11-14 21:07:23 +0300

    Added to app description - author

---
+++ src/components/app-description/__author.js
@@ -0,0 +1,15 @@
+/**
+ * __author module.
+ * @module simple-spa-js/app-descpription/__author.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const authorElement = {
+  /**
+   * Item description information about the author.
+   * @type {DOM element}
+   */
+  domElementTag: 'h6',
+  domElementClasses: ['app-description__author'],
+  domElementText: 'by Valeriy' 
+};
