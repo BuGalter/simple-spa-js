709740e544f8995900e5d3cc7104d08cf3b16a95
BuGalter <wardoc78@gmail.com>
2021-11-14 20:58:55 +0300

    Change short info about app

--- src/components/app-description/__short-info.js
+++ src/components/app-description/__short-info.js
@@ -4,7 +4,7 @@
  * @see module: simple-spa-js/dom-element.
  */
 
-export const shortInfo = {
+export const shortInfoElement = {
   /**
    * Item description short information about the application.
    * @type {DOM element}
@@ -13,9 +13,6 @@ export const shortInfo = {
   domElementClasses: ['app-description__short-info'],
   domElementStyle: {
     'color': 'red;',
-  },
-  domElementAttributes: {
-    test: 'test h3',
   },
   domElementText: 'A simple single page application written in HTML, CSS, JS for working\
   with the database mySQL' 
