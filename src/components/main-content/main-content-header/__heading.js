4301621be4795a5efbe96c26029aa3d9fd5f8404
BuGalter <wardoc78@gmail.com>
2021-11-16 08:35:07 +0300

    Added description element heading conteiner main content header

---
+++ src/components/main-content/main-content-header/__heading.js
@@ -0,0 +1,15 @@
+/**
+ * __heading module.
+ * @module simple-spa-js/main-content/main-content-header/__heading.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const headingElement = {
+  /**
+   * Item description heading main content table.
+   * @type {DOM element}
+   */
+  domElementTag: 'h3',
+  domElementClasses: ['main-content-header__heading'],
+  domElementText: 'Таблица с данными о ???' 
+};
