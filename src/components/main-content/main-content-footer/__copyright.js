8b5656c3501b00349edfccceafd6be9a1df1b993
BuGalter <wardoc78@gmail.com>
2021-11-16 19:51:11 +0300

    Added to main content footer element copyright

---
+++ src/components/main-content/main-content-footer/__copyright.js
@@ -0,0 +1,15 @@
+/**
+ * __copyright module.
+ * @module simple-spa-js/main-content/main-content-footer/__copyright.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const copyrightElement = {
+  /**
+   * Item description copyright and year in footer app.
+   * @type {DOM element}
+   */
+  domElementTag: 'span',
+  domElementClasses: ['main-content-footer__copyright'],
+  domElementText: '&copy 2021' 
+};
