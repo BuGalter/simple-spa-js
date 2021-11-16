e1088fc97b637d42668f8d454bb1b2a14a87dbea
BuGalter <wardoc78@gmail.com>
2021-11-16 08:58:25 +0300

    Added to index.js conteiner main content

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './index.css';
 import { makeAppDescription } from './components/app-description/app-description';
 import { makeAppHeader } from './components/app-header/app-header';
+import { makeMainContent } from './components/main-content/main-content';
 
 const body = document.body
 const appHeader = makeAppHeader();
 body.prepend(appHeader);
 const appDescription = makeAppDescription();
 const wrapper = document.querySelector('.wrapper-content')
-wrapper.prepend(appDescription);
+wrapper.append(appDescription);
+const mainContent = makeMainContent();
+wrapper.append(mainContent);
+
