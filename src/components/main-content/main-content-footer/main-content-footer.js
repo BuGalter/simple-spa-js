df98a533a287a023adf92df01c47eb9944135098
BuGalter <wardoc78@gmail.com>
2021-11-16 19:45:54 +0300

    Added to main content footer

---
+++ src/components/main-content/main-content-footer/main-content-footer.js
@@ -0,0 +1,25 @@
+import { Creator } from "../../../utils/creator";
+
+/**
+ * main-content-footer module.
+ * @module simple-spa-js/main-content/main-content-footer.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+
+ const mainContentFooterConteiner = {
+  /**
+   * Description of the container that will contain footer app.
+   * @type {DOM element}
+   */
+  domElementTag: 'div',
+  domElementClasses: ['main-content-footer', 'flex-block'],
+  domElementStyle: {
+    'background-color': 'black;',
+  }
+};
+
+function makeMainContentFooter() {
+};
+
+export { makeMainContentFooter };
