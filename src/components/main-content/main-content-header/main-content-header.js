68e5d78a44cd0120b856730cb091165812865115
BuGalter <wardoc78@gmail.com>
2021-11-16 08:28:38 +0300

    Added description conteiner to main content header

---
+++ src/components/main-content/main-content-header/main-content-header.js
@@ -0,0 +1,15 @@
+/**
+ * main-content-header module.
+ * @module simple-spa-js/main-content/main-content-header.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+
+ const mainContentHeaderConteiner = {
+  /**
+   * Description of the container that will contain information about table.
+   * @type {DOM element}
+   */
+  domElementTag: 'div',
+  domElementClasses: ['main-content-header'],
+};
