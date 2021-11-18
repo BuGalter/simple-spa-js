975d01de04ec5759c289766f8fe82e4e68f699b0
BuGalter <wardoc78@gmail.com>
2021-11-18 18:57:12 +0300

    Added tablecell element

---
+++ src/components/main-content/main-content-table/table-data/table-cell.js
@@ -0,0 +1,17 @@
+/**
+ * table-cell module.
+ * @module simple-spa-js/main-content/main-content-table/table-data/table-cell.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const tableCellElement = {
+  /**
+   * Item description table cell.
+   * @type {DOM element}
+   */
+  domElementTag: 'td',
+  domElementClasses: ['table-cell'],
+  domElementStyle: {
+    'border': '1px solid grey;',
+  }
+};
