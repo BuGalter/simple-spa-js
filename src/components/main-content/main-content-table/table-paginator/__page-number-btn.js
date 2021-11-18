c3c405efeab103bfc5d8020f917a777545d72069
BuGalter <wardoc78@gmail.com>
2021-11-18 17:15:38 +0300

    Added page number button to paginator

---
+++ src/components/main-content/main-content-table/table-paginator/__page-number-btn.js
@@ -0,0 +1,19 @@
+/**
+ * __page-number-btn module.
+ * @module simple-spa-js/main-content/main-content-table/table-paginator/__page-number-btn.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const pageNumberBtnElement = {
+  /**
+   * Item description paginator button.
+   * @type {DOM element}
+   */
+  domElementTag: 'button',
+  domElementClasses: ['table-paginator__page-number-btn'],
+  domElementStyle: {
+    'border-radius': '10px;',
+    'background': 'green;',
+    'color': 'gold;'
+  }
+};
