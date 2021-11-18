cc2f1891008e805334bf38d58f17849cc2f17e77
BuGalter <wardoc78@gmail.com>
2021-11-18 18:03:44 +0300

    Change table head element on headers

---
+++ src/components/main-content/main-content-table/table-data/table-headers.js
@@ -0,0 +1,14 @@
+/**
+ * table-headers module.
+ * @module simple-spa-js/main-content/main-content-table/table-data/table-headers.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const tableHeadersElement = {
+  /**
+   * Item description table head element.
+   * @type {DOM element}
+   */
+  domElementTag: 'th',
+  domElementClasses: ['table-headers'],
+};
