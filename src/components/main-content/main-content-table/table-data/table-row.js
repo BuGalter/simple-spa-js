b6c931f3483f0c2eb342ace6066345875b49dfb1
BuGalter <wardoc78@gmail.com>
2021-11-18 17:57:25 +0300

    Added table row element for table

---
+++ src/components/main-content/main-content-table/table-data/table-row.js
@@ -0,0 +1,14 @@
+/**
+ * table-row module.
+ * @module simple-spa-js/main-content/main-content-table/table-data/table-row.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const tableRowElement = {
+  /**
+   * Item description table row.
+   * @type {DOM element}
+   */
+  domElementTag: 'tr',
+  domElementClasses: ['table-row'],
+};
