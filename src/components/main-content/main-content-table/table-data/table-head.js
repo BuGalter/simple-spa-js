812ee75fc0545db765c2e753873dda18c459bc44
BuGalter <wardoc78@gmail.com>
2021-11-18 17:59:36 +0300

    Added table head element for table

---
+++ src/components/main-content/main-content-table/table-data/table-head.js
@@ -0,0 +1,14 @@
+/**
+ * table-head module.
+ * @module simple-spa-js/main-content/main-content-table/table-data/table-head.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const tableHeadElement = {
+  /**
+   * Item description table head element.
+   * @type {DOM element}
+   */
+  domElementTag: 'th',
+  domElementClasses: ['table-head'],
+};
