1a18e9dfc44907c6d7f6b98f05560030796af3e2
BuGalter <wardoc78@gmail.com>
2021-11-18 17:39:29 +0300

    Added description of caption for table

---
+++ src/components/main-content/main-content-table/table-data/__caption.js
@@ -0,0 +1,18 @@
+/**
+ * __caption module.
+ * @module simple-spa-js/main-content/main-content-table/table-data/__caption.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const captionTableElement = {
+  /**
+   * Item description caption for table.
+   * @type {DOM element}
+   */
+  domElementTag: 'caption',
+  domElementClasses: ['data-table__caption'],
+  domElementStyle: {
+    'color': 'gold;',
+  },
+  domElementText: 'Перечень товаров.' 
+};
