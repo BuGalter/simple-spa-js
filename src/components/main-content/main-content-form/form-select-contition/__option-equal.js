73ffbbb23d0c743fd3329bffe36c2b20da8da66d
BuGalter <wardoc78@gmail.com>
2021-11-23 18:25:03 +0300

    Added to select condition option equal

---
+++ src/components/main-content/main-content-form/form-select-contition/__option-equal.js
@@ -0,0 +1,19 @@
+/**
+ * __option-equal module.
+ * @module simple-spa-js/main-content/main-content-form/form-select-column/__option-equal.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const optionEqualElement = {
+  /**
+   * Item description option equal in select condition.
+   * @type {DOM element}
+   */
+  domElementTag: 'option',
+  domElementClasses: ['select-contition__option-equal'],
+  domElementAttributes: {
+    value: 'equal',
+    selected: true
+  },
+  domElementText: 'Равно'
+};
