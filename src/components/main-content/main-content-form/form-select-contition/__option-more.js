789e3d6bb352b8870ed8d92676b758701ea0c9dd
BuGalter <wardoc78@gmail.com>
2021-11-23 18:30:05 +0300

    Added to select condition option more

---
+++ src/components/main-content/main-content-form/form-select-contition/__option-more.js
@@ -0,0 +1,19 @@
+/**
+ * __option-more module.
+ * @module simple-spa-js/main-content/main-content-form/form-select-column/__option-more.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const optionMoreElement = {
+  /**
+   * Item description option more in select condition.
+   * @type {DOM element}
+   */
+  domElementTag: 'option',
+  domElementClasses: ['select-contition__option-more'],
+  domElementAttributes: {
+    value: 'more',
+    selected: true
+  },
+  domElementText: 'Больше'
+};
