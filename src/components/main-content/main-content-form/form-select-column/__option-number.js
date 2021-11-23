5898772863a768ee3a93106134ea1d36615bf093
BuGalter <wardoc78@gmail.com>
2021-11-23 17:21:04 +0300

    Added option number to select column

---
+++ src/components/main-content/main-content-form/form-select-column/__option-number.js
@@ -0,0 +1,18 @@
+/**
+ * __option-number module.
+ * @module simple-spa-js/main-content/main-content-form/form-select-column/__option-number.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const optionNumberElement = {
+  /**
+   * Item description option number in select column.
+   * @type {DOM element}
+   */
+  domElementTag: 'option',
+  domElementClasses: ['select-column__option-number'],
+  domElementAttributes: {
+    value: 'number',
+  },
+  domElementText: 'Количество'
+};
