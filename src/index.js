8aeb0d5c53ecf4dc445fb1cefadf0a37531ae58c
BuGalter <wardoc78@gmail.com>
2021-12-09 21:18:18 +0300

    Remove footer to wrapper content

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './index.css';
-import { Creator } from './utils/creator';
 import { makeAppDescription } from './components/app-description/app-description';
 import { makeAppHeader } from './components/app-header/app-header';
 import { makeMainContent } from './components/main-content/main-content';
+import { makeMainContentFooter } from './components/main-content/main-content-footer/main-content-footer';
 
 const body = document.body
 const appHeader = makeAppHeader();
@@ -12,6 +12,8 @@ const wrapper = document.querySelector('.wrapper-content')
 wrapper.append(appDescription);
 const mainContent = makeMainContent();
 wrapper.append(mainContent);
+const footer = makeMainContentFooter();
+wrapper.append(footer);
 
 let url = 'http://127.0.0.1:7000/api';
 let response = await fetch(url, {
