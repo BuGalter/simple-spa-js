d5a389b3ac24fbe3b150a56d176959208fda28b8
BuGalter <wardoc78@gmail.com>
2021-12-10 13:45:00 +0300

    Added to index.js importts elements for fill data table

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import { makeAppDescription } from './components/app-description/app-description
 import { makeAppHeader } from './components/app-header/app-header';
 import { makeMainContent } from './components/main-content/main-content';
 import { makeAppFooter } from './components/app-footer/main-content-footer';
+import { tableRowElement } from './components/main-content/main-content-table/table-data/table-row';
+import { tableCellElement } from './components/main-content/main-content-table/table-data/table-cell';
+import { getApiData } from './utils/get-data-api';
+import { insertDataTable } from './utils/insert-data-table';
 
 const body = document.body
 const appHeader = makeAppHeader();
 body.prepend(appHeader);
 const appDescription = makeAppDescription();
-const wrapper = document.querySelector('.wrapper-content')
+const wrapper = document.querySelector('.wrapper-content');
 wrapper.append(appDescription);
 const mainContent = makeMainContent();
 wrapper.append(mainContent);
 const footer = makeAppFooter();
 wrapper.append(footer);
+const table = document.querySelector('.table-data');
