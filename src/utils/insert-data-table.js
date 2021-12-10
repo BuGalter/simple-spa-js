596e04ed2fa5247e392f7e5e79adeee7ce794d99
BuGalter <wardoc78@gmail.com>
2021-12-10 13:51:34 +0300

    Added imports creator and element to insert-data-table.js

--- src/utils/insert-data-table.js
+++ src/utils/insert-data-table.js
@@ -1,3 +1,7 @@
+import { Creator } from "./creator";
+import { tableRowElement } from "../components/main-content/main-content-table/table-data/table-row";
+import { tableCellElement } from "../components/main-content/main-content-table/table-data/table-cell";
+
 function insertDataTable(data, container) {
   let len = data.length;
   for (let i = 0; i < len; i += 1) {
