74d96f57f98c3ca089644b7f46fcbb1ad764ce79
BuGalter <wardoc78@gmail.com>
2021-12-10 13:06:11 +0300

    Added to function insertDataTable correct data display

--- src/components/main-content/main-content-table/table-data/table-data.js
+++ src/components/main-content/main-content-table/table-data/table-data.js
@@ -38,10 +38,12 @@ function insertDataTable(data, conteiner) {
   let len = data.length;
   for (let i = 0; i < len; i += 1) {
     let tableRowData = Creator.collectDomElement(tableRowElement);
-    for (let j = 0; j < 4; j += 1) { 
-      let tableCell = Creator.collectDomElement(tableCellElement);
-      tableCell.textContent = data[i].date;
-      tableRowData.append(tableCell);
+    for (let key in data[i]) { 
+      if (key !== 'id') {
+        let tableCell = Creator.collectDomElement(tableCellElement);
+        tableCell.textContent = data[i][key];
+        tableRowData.append(tableCell);
+      };
     };
     conteiner.append(tableRowData);
   };
