cdfc63a202a5d307766437e70a9879798e61437a
BuGalter <wardoc78@gmail.com>
2021-12-15 21:25:31 +0300

    Added to table body element

---
+++ src/components/main-content/main-content-table/table-data/table-tbody.js
@@ -0,0 +1,14 @@
+/**
+ * table-tdody module.
+ * @module simple-spa-js/main-content/main-content-table/table-data/table-tbody.
+ * @see module: simple-spa-js/dom-element.
+ */
+
+ export const tableTbodyElement = {
+  /**
+   * Item description table body.
+   * @type {DOM element}
+   */
+  domElementTag: 'tbody',
+  domElementClasses: ['table-body-data'],
+};
